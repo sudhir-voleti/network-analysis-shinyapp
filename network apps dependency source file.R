acc2e7f92a9102887dddba0c7eb4d00ae0b2d116
yogesh1612 <yogesh1612@users.noreply.github.com>
2019-10-29 16:23:17 +0530

    Add files via upload

---
+++ network apps dependency source file.R
@@ -0,0 +1,15 @@
+## source file for network apps
+
+if (!require(igraph)) {install.packages("igraph")};  library(igraph)
+
+# if (!require(qgraph)) {install.packages("qgraph")};  library(qgraph)
+
+if (!require(ggraph)) {install.packages("ggraph")};  library(ggraph)
+
+# if (!require(sna)) {install.packages("sna")};  library(sna)
+
+# if (!require(tidyverse)) {install.packages("tidyverse")};  library(tidyverse)
+
+if (!require(shiny)) {install.packages("shiny")};  library(shiny)
+
+if (!require(visNetwork)) {install.packages("visNetwork")};  library(visNetwork)
